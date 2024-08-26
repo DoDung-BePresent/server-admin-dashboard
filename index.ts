58b0ebf8aa00b765350f1ffdeaa6afaf02c62bea
DoDung <doquangdung1782004@gmail.com>
2024-08-26 18:25:06 +0700

    run server

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { error } from "console";
+
 dotenv.config();
 
 const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.hcd7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at http://localhost:${process.env.PORT}`);
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURL);
+    console.log(`Connect to db successfully`);
+  } catch (error) {
+    console.log(`Can not connect to db ${error}`);
+  }
+};
 
-console.log("Hello");
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
